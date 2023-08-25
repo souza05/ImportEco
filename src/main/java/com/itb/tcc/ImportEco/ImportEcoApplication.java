d90537745e93d389b71dec4e127d07b90a92f809
Paulo Henrique Bezerra de Souza <rm86185@estudante.fieb.edu.br>
2023-08-25 12:03:31 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/ImportEco/ImportEcoApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.ImportEco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportEcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportEcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/ImportEco/ImportEcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.ImportEco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportEcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
