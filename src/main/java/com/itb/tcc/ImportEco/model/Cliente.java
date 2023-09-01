985c51497f238695f12f561c2810ea95b6c904fd
Paulo Henrique Bezerra de Souza <rm86185@estudante.fieb.edu.br>
2023-09-01 12:22:25 -0300

    camada model concluída

---
+++ src/main/java/com/itb/tcc/ImportEco/model/Cliente.java
@@ -0,0 +1,129 @@
+package com.itb.tcc.ImportEco.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Auto incremento do SQL SERVER
+	
+	private Long id;
+	private String email;
+	private String senha;
+	private String nome;
+	private String telefone;
+	private String idade;
+	private String logradouro; // rua, avenida, viela
+	private String cep;
+	private String bairro;
+	private String cidade;
+	private LocalDateTime dataNascimento;
+	
+	private boolean codStatusCliente;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getIdade() {
+		return idade;
+	}
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public LocalDateTime getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDateTime dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public boolean isCodStatusCliente() {
+		return codStatusCliente;
+	}
+
+	public void setCodStatusCliente(boolean codStatusCliente) {
+		this.codStatusCliente = codStatusCliente;
+	}
+}
+
+	
